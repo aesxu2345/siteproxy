7b31deed2ff75320585d5013cf202d29f6e3b2ad
aesxu2345 <87484439+aesxu2345@users.noreply.github.com>
2023-05-20 11:59:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.4everland.app.now.sh',
+    serverName: 'siteproxy.4everland.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
